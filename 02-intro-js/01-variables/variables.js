e05c6b7d8c2b4a57ff3051c060b0c414faf62ad0
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-03-20 00:24:47 -0600

    Variables and data types.

---
+++ 02-intro-js/01-variables/variables.js
@@ -0,0 +1,58 @@
+console.log("******** Variables ********");
+/* 
+  Scope / alcance / (ambiente)
+  global
+*/
+// Declarar una variable:
+// 1. crear la variable con su nombre
+// 2. asignarle un valor (están inicializadas)
+var nombreMujer = "Sara";
+var nombreHombre = "Matías";
+var nombre; // var es viejito
+// let es el nuevo var
+let mascota = "perro";
+// Declarar una (variable) constante
+/*
+  No es posible asignar otro valos a una variable de tipo constante.
+*/
+const diaLunes = "Lunes";
+const LUNES = "Lunes";
+
+console.log("Algunos nombres más comunes en Colombia:");
+console.log(nombreMujer);
+console.log(nombreHombre);
+console.log(mascota);
+
+console.log("Otros nombres comunes en Colombia:");
+// actualizar el contenido de las variables:
+nombreMujer = "María";
+nombreHombre = "Felipe";
+mascota = "gato";
+
+console.log(nombreMujer);
+console.log(nombreHombre);
+console.log(mascota);
+
+
+
+// diaLunes = "Miércoles";  // Esto genera error
+
+console.log(diaLunes,nombreMujer);
+
+// Bloques de código 
+{  // Este bloque tiene un escope/alcance menor al globlal
+  let fruta = "plátano";
+  console.log("Algunos nombres no tan comunes en Colombia:");
+  nombreMujer = "Linhet";
+  nombreHombre = "Heladio";
+  mascota = "Oso polar";
+
+  console.log(nombreMujer); // var
+  console.log(nombreHombre);// var
+  console.log(mascota); // let
+
+  // let fruta = "plátano";
+  console.log(fruta); // let
+}
+
+console.log(mascota);
