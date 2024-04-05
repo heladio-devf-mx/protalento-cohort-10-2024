8cc8756001a930eb1edeaec99f5790eb8de3cec7
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-04-04 20:17:34 -0600

    Destructuring and Random button functionality.

--- 02-intro-js/09-objetos/objetos.js
+++ 02-intro-js/09-objetos/objetos.js
@@ -14,7 +14,8 @@ let alumno = {
   direccion: {
     calle: "Reforma",
     numeroExterior: 55,
-    cp: "05878"
+    cp: "05878",
+    pais: "Colombia"
   },
   // Métodos
   subirProyectoAlCampus: function(urlArchivo, nombreProyecto) {
@@ -25,6 +26,15 @@ let alumno = {
     // TO DO agregar fecha
   }
 };
+/*
+// Tradicional
+let cursosAlumno =  alumno.cursos;  // acceso tradicional
+cursosAlumno =  alumno["cursos"];  // acceso tradicional
+console.log(cursosAlumno);
+// Destructuting / Desestructuración
+const { cursos } = alumno;
+console.log(cursos);
+*/
 
 // Acceder a las propiedades del objeto
 // Notación punto
@@ -56,3 +66,48 @@ console.log(alumno["cursos"]);
 console.log(alumno["yaFinalizo"]);
 console.log(alumno["subirProyectoAlCampus"]("https://github.com/heladio-devf-mx/protalento-cohort-10-2024/blob/main/02-intro-js/08-arreglos/campus/ArreglosCampus-Ejercicio1.js", "Arreglos-Ejercicio1-corchetes"));
 
+// Alumnos presentes
+const studentsOnClass = [
+  'Andrés Muñoz', // 0
+  'Andrés Camilo', // 1
+  'Angela Cárdenas', // 2
+  'Brayan David', // 3
+  'Dayana Vanessa', // 4
+  'Deyber Andrés', // 5
+  'José Andrés', // 6
+  'José Enrique', // 7
+  'Margy Sarria', // 8
+  'Marycriz Chica', // 9
+  'María José', // 10
+  'Sergio Andrés', // 11
+  'Carlos Barragán', //12
+];
+// Tómbola para mostrar los objetos creados
+// Arrow Function
+const selectStudent = (students = studentsOnClass) => {
+  const positionSelected = Math.ceil(Math.random() * students.length);
+  console.log("Posición del alumno afortunado: " + positionSelected);
+  const luckyStudent = students[positionSelected];
+  console.log("Alumno afortunado: " + luckyStudent);
+  // Mostrarlo
+  const studentElement = document.querySelector('#student-name');
+  studentElement.textContent = luckyStudent;
+  return luckyStudent;
+}
+
+// let alumnoGanador = selectStudent();
+// console.log(alumnoGanador);
+
+
+// Tradicional
+let cursosAlumno =  alumno.cursos;  // acceso tradicional
+cursosAlumno =  alumno["cursos"];  // acceso tradicional
+console.log(cursosAlumno);
+// Destructuting / Desestructuración
+const { nombre, cursos, direccion } = alumno;
+console.log(cursos, direccion);
+
+const { pais, numeroExterior, calle, cp } = alumno.direccion;
+
+// Interpolación de cadena (cadenas de texto + código JS)
+console.log(`El alumno ${nombre} vive en la calle ${calle}, número ${numeroExterior}, cp ${cp}, en ${pais} y estudia ${cursos.join(', ')}`);
