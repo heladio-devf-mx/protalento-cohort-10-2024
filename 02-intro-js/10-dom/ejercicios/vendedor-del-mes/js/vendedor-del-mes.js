bbf8d67f1202e13698ba45ac3f7bcc7a5b8e0596
Mackaber <Mackaber@users.noreply.github.com>
2024-04-06 10:55:45 -0600

    codigo compacto para ejercicio vendedor del mes

--- 02-intro-js/10-dom/ejercicios/vendedor-del-mes/js/vendedor-del-mes.js
+++ 02-intro-js/10-dom/ejercicios/vendedor-del-mes/js/vendedor-del-mes.js
@@ -8,53 +8,52 @@ let precios = {
 let ventas_juana = {};
 let ventas_pedro = {};
 
-function agregarVentasJuana() {
-  console.log("agregando ventas Juana");
-
-  alert("Ingrese datos de venta de Juana");
+function preguntar() {
+  let ventas = {};
   let cantidad_aqua = prompt("Ingrese la cantidad vendida de Aqua");
   let cantidad_emocion = prompt("Ingrese la cantidad vendida de Emocion");
   let cantidad_alegria = prompt("Ingrese la cantidad vendida de Alegria");
   let cantidad_frescura = prompt("Ingrese la cantidad vendida de Frescura");
 
-  ventas_juana = {
-    aqua: cantidad_aqua,
-    emocion: cantidad_emocion,
-    alegria: cantidad_alegria,
-    frescura: cantidad_frescura,
-  };
+  ventas.aqua = cantidad_aqua;
+  ventas.emocion = cantidad_emocion;
+  ventas.alegria = cantidad_alegria;
+  ventas.frescura = cantidad_frescura;
 
+  return ventas // { aqua: 10, emocion: 20, alegria: 30, frescura: 40 }
+}
+
+function calcularVentas(ventas) {
+  ventas.aqua * precios.aqua; // cantidad * precio
+  let total_ventas = 0;
+  total_ventas += ventas.aqua * precios.aqua;
+  total_ventas += ventas.emocion * precios.emocion;
+  total_ventas += ventas.alegria * precios.alegria;
+  total_ventas += ventas.frescura * precios.frescura;
+  console.log(total_ventas);  
+
+  return total_ventas
+}
+ 
+function agregarVentasJuana() {
+  console.log("agregando ventas Juana");
+
+  alert("Ingrese datos de venta de Juana");
+  ventas_juana = preguntar();
 }
 
 function agregarVentasPedro() {
   console.log("agregando ventas Pedro");
 
   alert("Ingrese datos de venta de Pedro");
-  let cantidad_aqua = prompt("Ingrese la cantidad vendida de Aqua");
-  let cantidad_emocion = prompt("Ingrese la cantidad vendida de Emocion");
-  let cantidad_alegria = prompt("Ingrese la cantidad vendida de Alegria");
-  let cantidad_frescura = prompt("Ingrese la cantidad vendida de Frescura");
-
-  ventas_pedro.aqua = cantidad_aqua;
-  ventas_pedro.emocion = cantidad_emocion;
-  ventas_pedro.alegria = cantidad_alegria;
-  ventas_pedro.frescura = cantidad_frescura;
+  ventas_pedro = preguntar();
 }
 
 function calcularVendedorDelMes() {
   console.log("calculando vendedor del mes");
 
-  let total_ventas_juana =  ventas_juana.aqua * precios.aqua // cantidad * precio
-  total_ventas_juana += ventas_juana.emocion * precios.emocion
-  total_ventas_juana += ventas_juana.alegria * precios.alegria
-  total_ventas_juana += ventas_juana.frescura * precios.frescura
-  console.log(total_ventas_juana);  
-
-  let total_ventas_pedro = ventas_pedro.aqua * precios.aqua
-  total_ventas_pedro += ventas_pedro.emocion * precios.emocion
-  total_ventas_pedro += ventas_pedro.alegria * precios.alegria
-  total_ventas_pedro += ventas_pedro.frescura * precios.frescura
-  console.log(total_ventas_pedro);
+  let total_ventas_juana = calcularVentas(ventas_juana);
+  let total_ventas_pedro = calcularVentas(ventas_pedro);  
 
   // Hacer la comparación
   if(total_ventas_juana == total_ventas_pedro) {
