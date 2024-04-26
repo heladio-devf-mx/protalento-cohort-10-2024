f3efef0519a318f94ae8db806ba112d76bb8db71
Heladio Arteaga Franco <heladiofog@gmail.com>
2024-04-25 18:06:31 -0600

    Logica de inicio de sesión.

--- 02-intro-js/11-proyecto-cajero-automatico/login.js
+++ 02-intro-js/11-proyecto-cajero-automatico/login.js
@@ -5,12 +5,20 @@ console.log("Cajero de MEXICOL!");
  * Tabla
  */
 const usuarios = [
-    { id: 1, userName: "mali", password: "mali123" },
-    { id: 2, userName: "gera", password: "gera456" },
-    { id: 3, userName: "maui", password: "maui789" },
-    { id: 3, userName: "silvia", password: "Silv14" },
+  { id: 1, nombre: "Mali", userName: "mali", password: "mali123" },
+  { id: 2, nombre: "Gerardo", userName: "gera", password: "gera456" },
+  { id: 3, nombre: "Maui", userName: "maui", password: "maui789" },
+  { id: 3, nombre: "Silvia", userName: "silvia", password: "Silv14" },
 ];
 
+// const getUsuarios = () => {
+//   fetch("./bd/usuarios.json")
+//     .then((data) => {
+//       console.log(data);
+//       return data.json();
+//     })
+//     .then((jsonData) => console.log(jsonData));
+// };
 // Maps y Sets (Estructuras de datos)
 // TO DO: hacer un API para esas BDs
 
@@ -18,30 +26,68 @@ const usuarios = [
  * BD de cuentas
  */
 const cuentas = [
-    { userId: 1, nombre: "Mali", saldo: 200 },
-    { userId: 2, nombre: "Gera", saldo: 290 },
-    { userId: 3, nombre: "Maui", saldo: 67 }
+  { userId: 1, nombre: "Mali", saldo: 200 },
+  { userId: 2, nombre: "Gera", saldo: 290 },
+  { userId: 3, nombre: "Maui", saldo: 67 },
 ];
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    // console.log(event);
-    // 0. Ya se cargó el (contenido) DOM
+  // console.log(event);
+  // 0. Ya se cargó el (contenido) DOM
 
-    // 1. Lógica de login (negocio)
-    const loginForm = document.getElementById("login-form");
-    loginForm.addEventListener("submit", (event) => {
-        event.preventDefault(); // prevenir que se envíe el formulario
-        console.log("Intento de inicio de sesión...");
-        // 1. Recuperar la información que ingresó el usuario
+  // 1. Lógica de login (negocio)
+  const loginForm = document.getElementById("login-form");
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevenir que se envíe el formulario
+    console.log("Intento de inicio de sesión...");
+
+    // Capturamos usuario
+    const username = document.getElementById("username").value;
+    let resultado_container = document.getElementById("resultado_container");
+
+    // Validacion de usuario
+    if (
+      usuarios.find((usuario) => usuario.userName === username.toLowerCase().trim())
+    ) {
+      // Capturamos password
+      const passwordinput = document.getElementById("password").value;
+      const reguser = usuarios.find(
+        (usuario) => usuario.userName === username.toLowerCase().trim()
+      );
+      // Validar la contraseña
+      if (
+        usuarios.find(
+          (usuario) =>
+            usuario.userName === username.toLowerCase().trim() &&
+            usuario.password === passwordinput.trim()
+        )
+      ) {
+        console.log("Inicio de sesión exitoso\nBienvenido " + reguser.nombre);
+
+        resultado_container.innerHTML = `<p>Bienvenido ${reguser.nombre}</p>`;
+        // limpiar el formulario
         
-        // 1.1 Validar que los campos no estén vacíos
+      } else {
+        console.log("Contraseña incorrecta");
+        resultado_container.innerHTML = `<p>Contraseña incorrecta</p>`;
+        document.getElementById("password").value = ""; // limpiamos el valor del input
+      }
+    } else {
+      console.log("Usuario no encontrado");
 
-        // 2. Validar user name.
+      resultado_container.innerHTML = `
+            <p>Usuario no encontrado</p>`;
+    }
+    // 1. Recuperar la información que ingresó el usuario
 
-        // 3. Validar contraseña.
+    // 1.1 Validar que los campos no estén vacíos
 
-        // 4. Si no son correctos usuario o contraseña, mostrar error.        
-        
-        // 5. Si son válidos sus datos, permitirle el acceso a su cuenta. 
-    });
+    // 2. Validar user name.
+
+    // 3. Validar contraseña.
+
+    // 4. Si no son correctos usuario o contraseña, mostrar error.
+
+    // 5. Si son válidos sus datos, permitirle el acceso a su cuenta.
+  });
 });
