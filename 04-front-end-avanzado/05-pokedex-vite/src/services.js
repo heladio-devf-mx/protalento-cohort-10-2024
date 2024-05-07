84ce192e7718f8b9ec672558d52510d92c718ddd
helladyo <heladiofog@gmail.com>
2024-05-06 22:05:15 -0600

    Adding another service to consume pokeapi.

--- 04-front-end-avanzado/05-pokedex-vite/src/services.js
+++ 04-front-end-avanzado/05-pokedex-vite/src/services.js
@@ -19,4 +19,26 @@ async function first150Pokemons() {
   return pokemons;
 }
 
+function getFirst50Pokemons() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
+    .then(response => {
+      // si salió bien con response.ok
+      return response.json(); // con esta llamada se devuelve otra promesa
+    })
+    .then(async (pokemons) => {
+      // console.log(pokemons);
+      let pokemonList = [];
+      for (let i = 0; i < pokemons.results.length; i++) {
+        let pokemon = await fetch(pokemons.results[i].url);
+        let pokemonDetalle = await pokemon.json();
+        // let nuevoPokemon = new Pokemon(pokemonDetalle.name, pokemonDetalle.sprites.other["official-artwor"].front_deafult)
+          console.log(pokemonDetalle);
+        pokemonList[i] = pokemonDetalle;
+      }
+    })
+    .catch(error => console.log("Error: " + error));
+}
+
+
+
 export { findPokemonByName, first150Pokemons };
