5d8aa2e5dcf5cdf63eb12a577485cd7368d675a9
Mackaber <Mackaber@users.noreply.github.com>
2024-05-28 21:15:17 -0600

    return jwt to the user

--- 05-backend/03-mvc/backend/controllers/authController.js
+++ 05-backend/03-mvc/backend/controllers/authController.js
@@ -1,11 +1,16 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config(); // Configurar dotenv
+
 
 const register = async (req, res) => {
   const { email, password } = req.body;
   encryptPassword = await bcrypt.hash(password, 10);
   const user = await User.create({ email, encryptPassword });
-  res.send(user);
+  res.status(201).send({ message: "Usuario creado correctamente" });
 }
 
 const login = async (req, res) => {
@@ -15,7 +20,11 @@ const login = async (req, res) => {
   if (user) {
     bcrypt.compare(password, user.encryptPassword, (err, result) => {
       if (result) {
-        res.send({message: "Login correcto"}); // Mandar el JWT
+        // res.send({message: "Login correcto"}); // Mandar el JWT
+        // Crear token
+        const token = jwt.sign({ id: user._id, email: user.email, role: user.role }, process.env.SECRET);
+
+        res.send({ message: "Login correcto", token });
       } else {
         res.status(401).send({ message: "Password incorrecto" });
       }
